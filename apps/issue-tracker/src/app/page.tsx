2d5d9eababa04cbea526f9e5efec26a9089f855b
Milind Wankhede <notyeninja@gmail.com>
2025-08-22 16:41:16 -0700

    added placeholder for issue tracker app

---
+++ apps/issue-tracker/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div>
+
+    </div>
+  );
+}
