d7333c06fa84f8532d60c40b6c44d6c84fc87fef
Milind Wankhede <notyeninja@gmail.com>
2025-11-21 21:43:50 -0800

    layout

--- apps/ticketor/src/app/(marketing)/layout.tsx
+++ apps/ticketor/src/app/(marketing)/layout.tsx
@@ -9,8 +9,8 @@ export default function MarketingLayout({children}: PropsWithChildren) {
     return (
         <div>
             <div>{children}</div>
-            <footer className="bg-fuchsia-300 p-2">
-                <p>
+            <footer className="p-2 border-t border-x-gray-200 dark:border-gray-600">
+                <p className="dark:text-gray-400">
                     &copy; <CopyrightYear />  Mode. Build for Next.js Sandbox
                 </p>
             </footer>
